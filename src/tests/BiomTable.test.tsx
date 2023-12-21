c134569251a6b6679973aeae4869d37229185e5f
svirgunvolodia <svirgun200@gmail.com>
2023-12-21 13:54:29 +0100

    add types/css-modules package & import jest-dom in test file

--- src/tests/BiomTable.test.tsx
+++ src/tests/BiomTable.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '@testing-library/jest-dom';
+
 import { render } from '@testing-library/react';
 import BiomTable from '../components/BiomTable';
 import axeTest from '../utils/testAxe';
